15381b64936c6fe5e150371b6edc8be6a7ae0868
ciprianotoor <ciprianotoor@gmail.com>
2025-11-07 13:39:37 -0600

    Reloj

---
+++ reloj.bat
@@ -0,0 +1,11 @@
+@echo off
+powershell -NoProfile -Command ^
+"while ($true) { ^
+    Clear-Host; ^
+        $hora = Get-Date -Format 'HH:mm:ss'; ^
+            $anchura = $Host.UI.RawUI.WindowSize.Width; ^
+                $espacios = (' ' + $hora).PadLeft([math]::Floor($anchura/2)+$hora.Length/2); ^
+                    Write-Host $espacios -ForegroundColor Cyan; ^
+                        Start-Sleep -Seconds 1 ^
+                        }"
+                        
